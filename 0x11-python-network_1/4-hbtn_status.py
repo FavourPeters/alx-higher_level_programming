cee4c4a33821619a098daa958d88a6d1c965ee76
FavourPeters <favourpeters1@gmail.com>
2023-05-02 08:57:46 +0100

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status."""
+"""fetches http://0.0.0.0:5050/status."""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    r = requests.get("http://0.0.0.0:5050/status")
     print("Body response:")
     print("\t- type: <class 'str'>".format(type(r.text)))
     print("\t- content: Custom status".format(r.text))
