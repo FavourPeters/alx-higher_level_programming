e2e9c493a4442efc0e4495bbac1d335535f93854
FavourPeters <favourpeters1@gmail.com>
2023-05-02 08:55:08 +0100

    updates

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("http://0.0.0.0:5050/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: <class 'str'>".format(type(r.text)))
     print("\t- content: Custom status".format(r.text))
